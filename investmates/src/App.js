54ad0ba42005228ba0101ffc6df5c2ec63fb7083
overthemoon11 <shu.hui051603@gmail.com>
2024-03-19 20:22:45 +0800

    UPDATE

--- investmates/src/App.js
+++ investmates/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/Header.js';
+import NewsCategoryPage from './components/Body/NewsCategoryPage.js';
 
 function App() {
   return (
