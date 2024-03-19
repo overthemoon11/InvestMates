11754da1ab3c46de32f6bd2d860fa03c631dc427
overthemoon11 <shu.hui051603@gmail.com>
2024-03-19 22:16:25 +0800

    Change to new branch

---
+++ investmates/src/News/Crypto.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './News.css';
+
+export const Crypto=()=>{
+    return (<aside id="left-section" className='left'>
+
+    <h1 className='pagetitle'>Crypto News</h1>
+    <h4 className='date'>- 10 March</h4>
+    Search NavBar
+    <nav>
+        <h4>LIKES</h4>
+        <ul>
+            <li><a href="#beginners" class="menu-item">Stock Market for Beginners</a></li>
+            <li><a href="#trading" class="menu-item">Simple Trading</a></li>
+        </ul>
+        <h4>COLLECTS</h4>
+        <ul>
+            <li><a href="#stocks" class="menu-item">Stocks & Investing</a></li>
+        </ul>
+        <h4>OTHERS</h4>
+    </nav>
+</aside>)
+};
