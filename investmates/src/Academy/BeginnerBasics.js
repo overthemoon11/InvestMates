155037adadda43b67eed452fde9a72848225cd08
XiaoXuan <hengxiaoxuan2003@gmail.com>
2024-03-23 11:38:18 +0800

    academy second commit

--- investmates/src/Academy/BeginnerBasics.js
+++ investmates/src/Academy/BeginnerBasics.js
@@ -1,5 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import LeftMenu from './LeftMenu';
+import RightComponent from './RightComponent.js';
+import LeftRightLayout from '../components/LeftRightColumn/LeftRightLayout.js';
+import IntroductionToTheCourse from './contents/IntroductionToTheCourse'
 
 export const Beginner=()=>{
-    return <h1>beginner</h1>
+    
+    const topics = ["Introduction to the Course",
+                    "What is an Investment",
+                    "Stocks can be good",
+                    "Investments",
+                    "Stock Exchange",
+                    "Index",
+                    "Methodologies of Indexes",
+                    "Introduction to American Stock",
+                    "Indices",
+                    "Stock Indexes",
+                    "Assets, Liabilities, and Equity"];
+
+    const [content, setContent] = useState(<IntroductionToTheCourse/>);
+
+    const changeContent = (content) => {
+        setContent(content)
+    };
+
+    return (
+      <div className="column-container">
+          <LeftRightLayout
+              leftComponent={<LeftMenu topics={topics} changeContent={changeContent} />}
+              rightComponent={<RightComponent content={content}/>}
+              />
+      </div>
+    ) 
 };
